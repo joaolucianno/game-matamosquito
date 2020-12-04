d5f79edd00df459fc4498c954f3548a5965a5d01
João Lucianno <joaolucianno@gmail.com>
2020-12-04 13:01:40 +0000

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 //Menu
 function verificarIniciar(){
-    let level = document.querySelector('#level');
-    if(level.value == 0){
+    let level = document.querySelector('#level').value;
+    if(level == 0){
         alert('Selecione um nível para iniciar o jogo');
     } else{
-        window.location.href="app.html?" + level.value;
-        console.log(level.value);
+        window.location.href="app.html?" + level;
+        //alert(level);
     }
 };
